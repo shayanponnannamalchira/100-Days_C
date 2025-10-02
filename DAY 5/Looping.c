91e7101dd484b3eca129f8636027e3ae1ac0a2e6
shayanponnannamalchira <shayan.malchira@s.amity.edu>
2025-10-02 15:26:03 +0530

    DAY 5

---
+++ DAY 5/Looping.c
@@ -0,0 +1,31 @@
+//For Loop
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    for(i=1; i<=10; i++)
+    {
+        printf("%d", i);
+    }
+}
+
+//While Loop
+int main() {
+    int a = 1;
+    while (a <= 10) {
+        printf("%d\n", a);
+        a++;
+    }
+    return 0;
+}
+
+//Do While Loop
+int main() {
+    int a = 1;
+    do {
+        printf("%d\n", a);
+        a++;
+    } while (a <= 10);
+    return 0;
+}
